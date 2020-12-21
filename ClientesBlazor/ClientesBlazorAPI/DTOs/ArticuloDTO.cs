485efb615cc95d2548f97f34c505cf9e5bdb9167
Jose <jose.berges23@gmail.com>
2020-12-20 23:07:18 -0400

    Add DTOs

---
+++ ClientesBlazor/ClientesBlazorAPI/DTOs/ArticuloDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientesBlazorAPI.DTOs
+{
+    public class ArticuloDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public decimal? Precio { get; set; }
+    }
+}
---
+++ ClientesBlazor/ClientesBlazorAPI/DTOs/ClienteDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientesBlazorAPI.DTOs
+{
+    public class ClienteDTO
+    {
+        public int Id { get; set; }
+        public int IdPais { get; set; }
+        public string Pais { get; set; }
+        public int IdGrupo { get; set; }
+        public string Grupo { get; set; }
+        public string Nombre { get; set; }
+        public string Rnc { get; set; }
+        public List<ArticuloDTO> Articulos { get; set; }
+    }
+}
