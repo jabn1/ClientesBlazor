d283d2794ebf6f743d7c55d0bcfffc305987987a
Jose <jose.berges23@gmail.com>
2020-12-21 00:12:38 -0400

    Add GetClientes endpoint

---
+++ ClientesBlazor/ClientesBlazorAPI/Controllers/ClientesController.cs
@@ -0,0 +1,80 @@
+﻿using ClientesBlazorAPI.DTOs;
+using Infraestructura.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientesBlazorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ClientesDBContext context;
+
+        public ClientesController(ClientesDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("clientes")]
+        public async Task<ActionResult<List<ClienteDTO>>> GetClientes()
+        {
+            var clientes = await context.TblClientes
+                .Include(g => g.IdGrupoNavigation)
+                .Include(p => p.IdPaisNavigation)
+                .Include(a => a.TblClienteArticulos)
+                .ToListAsync();
+
+            var clientesDTO = new List<ClienteDTO>();
+            foreach (var cliente in clientes)
+            {
+                var articulosDTO = new List<ArticuloDTO>();
+                foreach (var articulo in cliente.TblClienteArticulos)
+                {
+                    var articuloDTO = new ArticuloDTO() { 
+                        Id = articulo.IdArticuloNavigation.Id, 
+                        Nombre = articulo.IdArticuloNavigation.Nombre, 
+                        Codigo = articulo.IdArticuloNavigation.Codigo, 
+                        Precio = articulo.IdArticuloNavigation.Precio 
+                    };
+                    articulosDTO.Add(articuloDTO);
+                }
+                var clienteDTO = new ClienteDTO() { 
+                    Id = cliente.Id, 
+                    IdPais = cliente.IdPais,
+                    Pais = cliente.IdPaisNavigation.Nombre, 
+                    IdGrupo = cliente.IdGrupo, 
+                    Grupo = cliente.IdGrupoNavigation.Nombre, 
+                    Nombre = cliente.Nombre,
+                    Rnc = cliente.Rnc,
+                    Articulos = articulosDTO
+                };
+                clientesDTO.Add(clienteDTO);
+            }
+            return clientesDTO;
+        }
+
+        //[HttpGet("articulos")]
+        //public async Task<ActionResult<List<ArticuloDTO>>> GetArticulos()
+        //{
+
+        //}
+
+        //[HttpPost("articulos")]
+        //public async Task<ActionResult> AddArticulosToCliente(List<int> articulos)
+        //{
+
+        //}
+
+        //[HttpDelete("articulo")]
+        //public async Task<ActionResult> DeleteArticuloFromCliente(int articulo)
+        //{
+
+        //}
+    }
+}
--- ClientesBlazor/ClientesBlazorAPI/Startup.cs
+++ ClientesBlazor/ClientesBlazorAPI/Startup.cs
@@ -1,6 +1,8 @@
+using Infraestructura.Entidades;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,7 @@ namespace ClientesBlazorAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ClientesDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Connection1")));
             services.AddControllers();
         }
 
